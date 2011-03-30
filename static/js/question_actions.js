6c1c41095da6e5fb19804d01905aaab0f0600321
Peter Bengtsson <peter@fry-it.com>
2011-03-30 19:30:18 +0100

    reshuffled the static stuff

--- static/js/question_actions.js
+++
@@ -1,8 +0,0 @@
-head.ready(function() {
-   $('form#reject').submit(function() {
-      if ($('#reject_comment:hidden').size()) {
-         $('#reject_comment').show();
-         return false;
-      }
-   });
-});
