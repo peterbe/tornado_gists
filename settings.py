3029d83679b9d5e5fc18e9cda56833e404c28bd1
Peter Bengtsson <peter@fry-it.com>
2011-04-02 11:46:49 +0100

    added option to load local settings per developer

--- settings.py
+++ settings.py
@@ -14,15 +14,16 @@ COOKIE_SECRET = "Y2o1TzK2YQAGEYdkL5gmueJIFuY37EQm92XsTo1v/Wi="
 WEBMASTER = 'felinx.lee@gmail.com'
 ADMIN_EMAILS = ['felinx.lee@gmail.com']
 
-#TWITTER_CONSUMER_KEY = ''
-#TWITTER_CONSUMER_SECRET = open(os.path.join(_ROOT, 'twitter_consumer_secret')).read().strip()
-
 DEFAULT_DATABASE_NAME = 'tornadogists'
 
-oauth_settings = {
+OAUTH_SETTINGS = {
     'client_id': '505fee7706f024a2bcc1',
     'client_secret': open(os.path.join(_ROOT, 'github_client_secret')).read().strip(),
     'base_url': 'https://github.com/login/oauth/',
     'redirect_url': 'http://tornadogists.org/auth/github/' # can this be localhost:8000/...?
-    #'redirect_url': 'http://localhost:8000/auth/github/',
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
